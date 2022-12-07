bc71a4ccc210648923a1cf58a3323fbe9af37c92
actuallypav <actuallypav@gmail.com>
2022-12-07 23:12:20 +0000

    puzzle_1 complete

--- Day_3/puzzle_1.py
+++ Day_3/puzzle_1.py
@@ -1,10 +1,10 @@
-inputFile = open("input.txt", "r")
+inputFile = open("Day_3/input.txt", "r")
 Compartments = inputFile.readlines()
 sum_of_priorities = 0
 
 for compartment in Compartments:
     rucksack = compartment.replace("\n", "")
-    if len(rucksack != 0):
+    if len(rucksack) != 0:
         compartment_length = len(compartment) // 2
         compartment_one = rucksack[0:compartment_length]
         compartment_two = rucksack[-compartment_length : len(rucksack)]
@@ -13,5 +13,9 @@ for compartment in Compartments:
             if item in compartment_two:
                 if item.isupper() == True:
                     sum_of_priorities += ord(item) - 38
+                    break
                 else:
                     sum_of_priorities += ord(item) - 96
+                    break
+
+print("The sum of the priorities is: " + str(sum_of_priorities))

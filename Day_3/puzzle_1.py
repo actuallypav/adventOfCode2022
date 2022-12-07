2d42bd7209f42ad13c952de5166bec224ce8bb82
actuallypav <actuallypav@gmail.com>
2022-12-07 23:20:13 +0000

    comments and delete empty line in txt

--- Day_3/puzzle_1.py
+++ Day_3/puzzle_1.py
@@ -1,21 +1,29 @@
 inputFile = open("Day_3/input.txt", "r")
-Compartments = inputFile.readlines()
+Rucksacks = inputFile.readlines()
 sum_of_priorities = 0
 
-for compartment in Compartments:
-    rucksack = compartment.replace("\n", "")
+# get each rucksack one by one
+for rucksack in Rucksacks:
+    rucksack = rucksack.replace("\n", "")
+    # check the rucksack exists
     if len(rucksack) != 0:
-        compartment_length = len(compartment) // 2
-        compartment_one = rucksack[0:compartment_length]
-        compartment_two = rucksack[-compartment_length : len(rucksack)]
+        # divide the rucksack into compartments
+        rucksack_size = len(rucksack) // 2
+        compartment_one = rucksack[0:rucksack_size]
+        compartment_two = rucksack[-rucksack_size : len(rucksack)]
 
+        # check if item exists in both compartments
         for item in compartment_one:
             if item in compartment_two:
+                # check the priority of the item
                 if item.isupper() == True:
+                    # add the priority to the item
                     sum_of_priorities += ord(item) - 38
+                    # once share item is found exit loop
                     break
                 else:
                     sum_of_priorities += ord(item) - 96
                     break
 
+# output
 print("The sum of the priorities is: " + str(sum_of_priorities))

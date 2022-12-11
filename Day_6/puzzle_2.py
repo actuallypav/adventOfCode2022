6a260217ac9120dd2f7cf38a53cdf0682c70e843
actuallypav <actuallypav@gmail.com>
2022-12-11 23:34:57 +0000

    easy solve

---
+++ Day_6/puzzle_2.py
@@ -0,0 +1,17 @@
+inputFile = open("Day_6/input.txt", "r")
+Buffer = inputFile.read()
+no_before_start_packet = 0
+start_packet = []
+
+# loop throught the characters
+for char in Buffer:
+    if len(start_packet) < 14:
+        start_packet.append(char)
+        no_before_start_packet += 1
+    else:
+        if len(start_packet) == len(set(start_packet)):
+            print(no_before_start_packet)
+            break
+        start_packet.append(char)
+        start_packet.pop(0)
+        no_before_start_packet += 1
